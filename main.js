374ca7ddfc7e236c7cb962b72a8b7a9153307e60
diego gomes camargo <103039694+diegogoms@users.noreply.github.com>
2023-09-28 09:37:21 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+document.querySelector('.tecla_pom').onclick=tocaSomPom
+
+document.querySelector('#som_tecla_pom').play();
+
+function tocaSomPom ()[
+    document.querySelector('#som_tecla_pom').play():
