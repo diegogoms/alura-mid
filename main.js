693746608d084d7b30218a7a7c138a976f2a2919
diego gomes camargo <103039694+diegogoms@users.noreply.github.com>
2023-10-05 09:34:50 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -4,3 +4,8 @@ document.querySelector('#som_tecla_pom').play();
 
 function tocaSomPom ()[
     document.querySelector('#som_tecla_pom').play():
+ listaDeTeclas[0].onclick = TacaSomPom;
+
+ while(){
+
+}
